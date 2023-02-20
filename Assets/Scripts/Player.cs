cd88bc986ddc71faa4602eff3a00f3d7d3c8b7e3
ebubekir <ekaramustafa20@ku.edu.tr>
2023-02-21 00:21:01 +0300

    Collision Detection with Cast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,8 +15,56 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        gameObject.transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        
+        //use CapsuleCast instead of Raycast for better results
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, 
+                                            playerRadius, moveDir, moveDistance);
+        if (!canMove)
+        {
+            // Cannot move towards moveDir
+
+            //Check only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                                           playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                //Can only move on X axis
+                moveDir = moveDirX;
+
+            }
+            else
+            {
+                //Cannot move along the X axis
+                //Check only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                                               playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    //Can only move on Z axis
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //Cannot move in any direction
+                }
+            }
+        }
+        if (canMove)
+        {
+            //player does not collide with any other objects
+            gameObject.transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+        
+        
         isWalking = moveDir != Vector3.zero;
 
         //to rotate player, use Slerp to smooth rotation
