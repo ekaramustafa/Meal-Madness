5f8950ef775a16f8217d4704010d0e0585e8f5fd
ebubekir <ekaramustafa20@ku.edu.tr>
2023-02-21 00:56:52 +0300

    clear counter prefab added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,21 +7,61 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
     
-    private bool isWalking; 
+    private bool isWalking;
+    private Vector3 lastInteractDir; 
     private void Update()
     {
+        HandleMovement();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(moveDir  != Vector3.zero)
+        {
+            //store to use for raycast function.
+            //moveDir is a zero vector when the player does not move.
+            lastInteractDir = moveDir;
+        }
+        float interactDistance = 2f;
         
+        //Player cannot interact with object without moving if we use moveDir in raycast
+        bool canInteract = Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance,countersLayerMask);
+        if (canInteract)
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+        
+    }
+
+    private void HandleMovement()
+    {
+
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = 0.7f;
         float playerHeight = 2f;
-        
+
         //use CapsuleCast instead of Raycast for better results
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, 
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
                                             playerRadius, moveDir, moveDistance);
         if (!canMove)
         {
@@ -63,8 +103,8 @@ public class Player : MonoBehaviour
             //player does not collide with any other objects
             gameObject.transform.position += moveDir * moveSpeed * Time.deltaTime;
         }
-        
-        
+
+
         isWalking = moveDir != Vector3.zero;
 
         //to rotate player, use Slerp to smooth rotation
@@ -72,10 +112,7 @@ public class Player : MonoBehaviour
         transform.forward = Vector3.Slerp(transform.forward, -moveDir, Time.deltaTime * rotateSpeed);
 
 
-    }
 
-    public bool IsWalking()
-    {
-        return isWalking;
+
     }
 }
